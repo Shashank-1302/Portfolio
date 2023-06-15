4225520c0a8e00c39dd836b9109a0a86c86903b3
Shashank Shekhar <shashankshekhar1302@gmail.com>
2023-06-15 16:00:41 +0530

    project added

--- components/Projects.js
+++ components/Projects.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {projects} from '@/components/__projectDetails'
 
-const Projects = () => {
+const Project = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="bg-white rounded-xl py-10">
+      <div className="container mx-auto px-4">
+        <h2 className="text-4xl font-semibold text-black mb-4">Projects</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {projects.map((project, index) => (
+            <div key={index} className="bg-white p-4 rounded-xl shadow-md hover:shadow-xl hover:scale-105  ease-in duration-200">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-32 object-cover mb-4 rounded-lg"
+              />
+              <h3 className="text-xl font-semibold text-black mb-2">{project.title}</h3>
+              <p className="text-gray-600 mb-4">{project.description}</p>
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block bg-black hover:bg-blue-500 text-white px-4 py-2 rounded-xl transition-colors duration-300"
+              >
+                View Project
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Project;
