e8e42fc5bf96018e4b31a649fffdcf84eca9b98e
Shashank Shekhar <shashankshekhar1302@gmail.com>
2023-06-18 21:57:56 +0530

    projects updated

--- components/Details/__experinceDetails.js
+++ components/Details/__experinceDetails.js
@@ -1,15 +1,15 @@
 export const experiences = [
     {
       title: 'Full Stack Developer',
-      company: 'IPS Academy',
+      company: 'CMS IPS Academy',
       duration: '2022 - Present',
-      description: ' ',
+      description: '',
     },
     {
       title: 'Android Developer',
-      company: 'IPS Academy',
+      company: 'CMS IPS Academy',
       duration: '2021 - 2022',
-      description: ' ',
+      description: '',
     },
     // Add more experiences here
   ];
--- components/Details/__projectDetails.js
+++ components/Details/__projectDetails.js
@@ -1,28 +1,21 @@
 export const projects = [
     {
-      title: 'Project 1',
+      title: 'Wally',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       image: '/project1.jpg',
       link: 'https://example.com/project1',
     },
     {
-      title: 'Project 2',
+      title: 'Newstar',
       description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       image: '/project2.jpg',
       link: 'https://example.com/project2',
     },
     {
-      title: 'Project 3',
+      title: 'Finance Tracker',
       description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
       image: '/project3.jpg',
       link: 'https://example.com/project3',
-    },
-    {
-      title: 'Project 4',
-      description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      image: '/project4.jpg',
-      link: 'https://example.com/project4',
-      
     }
     // Add more projects as needed
   ];
